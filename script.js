f491313382247ddd70b1362b87beff7ed387a879
Ponderix <80052935+Ponderix@users.noreply.github.com>
2021-03-22 19:25:20 +0100

    Added and upsated stuff

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-
+//pan-zoom function
+var panZoomMap = svgPanZoom("#wahlkreise_map")
+svgPanZoom("#wahlkreise_map", {
+  viewportSelector: '.svg-pan-zoom_viewport'
+, panEnabled: true
+, controlIconsEnabled: false
+, zoomEnabled: true
+, dblClickZoomEnabled: true
+, mouseWheelZoomEnabled: true
+, preventMouseEventsDefault: true
+, zoomScaleSensitivity: 0.2
+, minZoom: 0.5
+, maxZoom: 10
+, fit: true
+, contain: false
+, center: true
+, refreshRate: 'auto'
+, beforeZoom: function(){}
+, onZoom: function(){}
+, beforePan: function(){}
+, onPan: function(){}
+, onUpdatedCTM: function(){}
+, customEventsHandler: {}
+, eventsListenerElement: null
+});
