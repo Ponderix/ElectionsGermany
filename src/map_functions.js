219467af46e782dc1982afd1a6fa4bd2da9f940d
Ponderix <80052935+Ponderix@users.noreply.github.com>
2021-04-15 14:40:10 +0200

    Added stuff

---
+++ src/map_functions.js
@@ -0,0 +1,10 @@
+//to get the data from the csv file
+/*d3.csv(["/map_data/wk17_prcsd", function(data) {
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i].Name);
+        console.log(data[i].Age);
+    }
+}]);*/
+
+//svg pan zoom
+var panZoom = svgPanZoom('#WahlkreiseMap');
